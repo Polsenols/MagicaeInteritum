0a90606c7d990b1070d71021df8e7c72641ea361
Polsenols <mads_did@hotmail.com>
2017-03-21 12:26:08 +0100

    Fixed

---
+++ Semester6_Game/Assets/Scripts/Player/PlayerHealth_NET.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth_NET : Photon.MonoBehaviour {
+
+    PhotonView m_PhotonView;
+    public int maxHealth;
+    private int health;
+
+    void Awake()
+    {
+        m_PhotonView = GetComponent<PhotonView>();
+    }
+
+    void Start()
+    {
+        //If this script is not on the local player, destroy it.
+    }
+
+    public int getHealth()
+    {
+       return health;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+            AttackShout();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+            Die();
+        Debug.Log("I now have health: " + health);
+    }
+
+    public void Die()
+    {
+        Debug.Log("I am dead xD");
+    }
+
+    void AttackShout()
+    {
+        if(m_PhotonView.isMine)
+        m_PhotonView.RPC("IWasAttacked", PhotonTargets.Others, PhotonNetwork.player.ID);
+    }
+
+    [PunRPC]
+    private void IWasAttacked(int playerID)
+    {
+        string log = "I was Attacked by " + playerID;
+        Debug.Log(log);
+        TakeDamage(10);
+    }
+}
---
+++ Semester6_Game/Assets/Scripts/Player/PlayerShooting_NET.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting_NET : Photon.MonoBehaviour {
+
+
+    private PhotonView m_PhotonView;
+    public GameObject testSpawn;
+	// Use this for initialization
+	void Awake () {
+        m_PhotonView = GetComponent<PhotonView>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SpawnShout();
+        }
+	}
+
+    void SpawnShout()
+    {
+            m_PhotonView.RPC("spawnThing", PhotonTargets.All, PhotonNetwork.player.ID);       
+    }
+
+    [PunRPC]
+    private void spawnThing(int playerID)
+    {
+        Instantiate(testSpawn, transform.position, Quaternion.identity);
+    }
+}
