05e5a737da2b16b32c558724faa7793f9adc6329
Polsenols <mads_did@hotmail.com>
2017-03-31 11:49:23 +0200

    Laser update

---
+++ Semester6_Game/Assets/Scripts/Environment/EnvironmentSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MovementEffects;
+
+public class EnvironmentSpawner : Photon.MonoBehaviour {
+
+    public GameObject LaserTower;
+    public Transform laserSpawnPos;
+	// Use this for initialization
+	void Start () {
+        if (PhotonNetwork.isMasterClient)
+        {
+            Timing.RunCoroutine(_SpawnElement(LaserTower, 5.0f, laserSpawnPos.position));
+        }
+	}
+	
+    IEnumerator<float> _SpawnElement(GameObject element, float waitTime, Vector3 pos)
+    {
+        yield return Timing.WaitForSeconds(waitTime);
+        PhotonNetwork.Instantiate(element.name, pos, Quaternion.identity, 0);
+    }
+}
--- Semester6_Game/Assets/Scripts/Environment/LaserTowerController.cs
+++ Semester6_Game/Assets/Scripts/Environment/LaserTowerController.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MovementEffects;
 
 public class LaserTowerController : MonoBehaviour {
     public float rotationSpeed;
+    public bool isActive = false;
+    public GameObject[] laser_rays;
 	// Use this for initialization
 	void Start () {
-		
+        Timing.RunCoroutine(_ActivateLaser(2.5f));
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if(isActive)
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (isActive)
+        {
+            PlayerHealth_NET playerHealth = other.GetComponent<PlayerHealth_NET>();
+            playerHealth.TakeDamage(playerHealth.maxHealth, -1, null);
+            Debug.Log("Hit player");
+
+        }
+    }
+
+    IEnumerator<float> _ActivateLaser(float waitTime)
+    {
+        yield return Timing.WaitForSeconds(waitTime);
+        for (int i = 0; i < laser_rays.Length; i++)
+        {
+            laser_rays[i].SetActive(true);
+        }
+        isActive = true;
+    }
 }
