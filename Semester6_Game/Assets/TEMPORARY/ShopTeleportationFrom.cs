194a74f0c2baa85d7ba55a05bfea152ef5012a5b
Martin Sørensen <martinsorensen1992@gmail.com>
2017-04-15 23:53:58 +0200

    Major level design; 3D models, textures, particle

---
+++ Semester6_Game/Assets/TEMPORARY/ShopTeleportationFrom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopTeleportationFrom : MonoBehaviour
+{
+
+    public GameObject[] teleportPointGameobjects = new GameObject[3];
+    private Vector3[] teleportTranformPoints = new Vector3[3];
+    private int teleportIndexer = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < teleportPointGameobjects.Length; i++)
+        {
+            teleportTranformPoints[i] = teleportPointGameobjects[i].GetComponent<Transform>().position;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Should be some code here that makes the player able to cast and lose hp again
+            Debug.Log("Teleporting player back...");
+            other.transform.position = teleportTranformPoints[teleportIndexer];
+            teleportIndexer++;
+            if (teleportIndexer == 3)
+            {
+                teleportIndexer = 0;
+            }
+        }
+    }
+}
