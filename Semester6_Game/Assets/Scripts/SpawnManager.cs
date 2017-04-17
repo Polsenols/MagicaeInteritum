279b573da7b684d33c13b6936ae01b806fccd9f7
Polsenols <mads_did@hotmail.com>
2017-04-17 23:17:27 +0200

    Balances and fixes

--- Semester6_Game/Assets/Scripts/SpawnManager.cs
+++ Semester6_Game/Assets/Scripts/SpawnManager.cs
@@ -13,6 +13,7 @@ public class SpawnManager : SingleTon<SpawnManager> {
     private Vector3 originalSpawnPos;
     public Light mainLight;
     public List<PlayerHealth_NET> Players = new List<PlayerHealth_NET>();
+    public Transform[] spawnPoints;
     private float originalSpawnRadius;
 
     void Start()
@@ -26,6 +27,11 @@ public class SpawnManager : SingleTon<SpawnManager> {
         Vector3 randomPos = Random.insideUnitSphere * spawnRadius;
         randomPos.y = spawnHeight;
         return randomPos;
+    }
+
+    public Vector3 GetRandomSpawn()
+    {
+        return spawnPoints[Random.Range(0, spawnPoints.Length)].position;
     }
 
     public void SetSpawnRadius(float radius)
--- Semester6_Game/Assets/Scripts/TeleportFromShop.cs
+++ Semester6_Game/Assets/Scripts/TeleportFromShop.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MovementEffects;
 
 public class TeleportFromShop : MonoBehaviour
 {
-
+    public float teleportOpenDelay;
     public GameObject[] teleportPointGameobjects = new GameObject[3];
     private Vector3[] teleportTranformPoints = new Vector3[3];
     private int indexer = 0;
@@ -13,18 +14,23 @@ public class TeleportFromShop : MonoBehaviour
     {
         for (int i = 0; i < teleportPointGameobjects.Length; i++)
             teleportTranformPoints[i] = teleportPointGameobjects[i].GetComponent<Transform>().position;
+
+        Timing.RunCoroutine(_EnablePortal());
+    }
+
+    IEnumerator<float> _EnablePortal()
+    {
+        transform.gameObject.SetActive(false);
+        yield return Timing.WaitForSeconds(teleportOpenDelay);
+        transform.gameObject.SetActive(true);
+
     }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player") && other.GetComponent<PhotonView>().isMine)
         {
-            other.transform.position = teleportTranformPoints[indexer];
-            indexer++;
-            if(indexer == 3)
-            {
-                indexer = 0;
-            }
+            other.transform.position = teleportTranformPoints[Random.Range(0,3)];
         }
     }
 
